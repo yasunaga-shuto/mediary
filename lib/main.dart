123d453318dab3905786281e1281bdd53ffbd146
yasunaga shuto <yasunaga.shuto@gmail.com>
2021-06-14 16:19:31 +0900

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import './medicines.dart';
+
+void main() => runApp(const Mediary());
+
+class Mediary extends StatelessWidget {
+  const Mediary({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mediary',
+      theme: ThemeData(
+        primaryColor: Colors.green,
+      ),
+      home: const Medicines(),
+    );
+  }
+}
