53903c900646b6ccb22d23b0ffa8b90c3a76ef96
yasunaga shuto <yasunaga.shuto@gmail.com>
2021-06-18 16:23:51 +0900

    fix main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,48 +3,10 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
-import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
-import 'models/medicine.dart';
 
 import 'home.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final database = openDatabase(
-    join(await getDatabasesPath(), 'mediary_database.db'),
-    onCreate: (db, version) {
-      return db.execute(
-        'CREATE TABLE medicines(id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER, takenAt TEXT)',
-      );
-    },
-    version: 1,
-  );
-
-  Future<void> insertMedicine(Medicine medicine) async {
-    final db = await database;
-
-    await db.insert('medicines', medicine.toMap(),
-        conflictAlgorithm: ConflictAlgorithm.replace);
-  }
-
-  Future<List<Medicine>> medicines() async {
-    final db = await database;
-
-    final List<Map<String, dynamic>> maps = await db.query('medicines');
-
-    return List.generate(maps.length, (i) {
-      return Medicine(
-        id: maps[i]['id'],
-        name: maps[i]['name'],
-        quantity: maps[i]['quantity'],
-        takenAt: maps[i]['takenAt'],
-      );
-    });
-  }
-
-  runApp(const Mediary());
-}
+void main() => runApp(const Mediary());
 
 class Mediary extends StatelessWidget {
   const Mediary({Key? key}) : super(key: key);
